dc073a41cc3a1f2ad9e1a0d6068085a00fd9bdd5
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-14 12:43:29 +0900

    Update bot-proxy.py

--- bot-proxy.py
+++ bot-proxy.py
@@ -15,17 +15,17 @@ import json
 
 class Birds:
     def __init__(self):
-        # Get file directory
+        # 파일 디렉토리 가져오기
         self.data_file = base.file_path(file_name="data-proxy.json")
         self.config_file = base.file_path(file_name="config.json")
 
-        # Initialize line
+        # 라인 초기화
         self.line = base.create_line(length=50)
 
-        # Initialize banner
+        # 배너 초기화
         self.banner = base.create_banner(game_name="Birds")
 
-        # Get config
+        # 설정 가져오기
         self.auto_do_task = base.get_config(
             config_file=self.config_file, config_name="auto-do-task"
         )
@@ -53,11 +53,11 @@ class Birds:
             accounts = json.load(open(self.data_file, "r"))["accounts"]
             num_acc = len(accounts)
             base.log(self.line)
-            base.log(f"{base.green}Number of accounts: {base.white}{num_acc}")
+            base.log(f"{base.green}계정 수: {base.white}{num_acc}")
 
             for no, account in enumerate(accounts):
                 base.log(self.line)
-                base.log(f"{base.green}Account number: {base.white}{no+1}/{num_acc}")
+                base.log(f"{base.green}계정 번호: {base.white}{no+1}/{num_acc}")
                 data = account["acc_info"]
                 proxy_info = account["proxy_info"]
                 parsed_proxy_info = base.parse_proxy_info(proxy_info)
@@ -71,49 +71,49 @@ class Birds:
                 try:
                     get_info(data=data, proxies=proxies)
 
-                    # Do task
+                    # 작업 수행
                     if self.auto_do_task:
-                        base.log(f"{base.yellow}Auto Do Task: {base.green}ON")
+                        base.log(f"{base.yellow}자동 작업 수행: {base.green}켜짐")
                         process_do_task(data=data, proxies=proxies)
                     else:
-                        base.log(f"{base.yellow}Auto Do Task: {base.red}OFF")
+                        base.log(f"{base.yellow}자동 작업 수행: {base.red}꺼짐")
 
-                    # Boost speed
+                    # 속도 부스트
                     if self.auto_boost_speed:
-                        base.log(f"{base.yellow}Auto Boost Speed: {base.green}ON")
+                        base.log(f"{base.yellow}자동 속도 부스트: {base.green}켜짐")
                         process_boost_speed(data=data, proxies=proxies)
                     else:
-                        base.log(f"{base.yellow}Auto Boost Speed: {base.red}OFF")
+                        base.log(f"{base.yellow}자동 속도 부스트: {base.red}꺼짐")
 
-                    # Mint worm
+                    # 지렁이 민팅
                     if self.auto_mint_worm:
-                        base.log(f"{base.yellow}Auto Mint Worm: {base.green}ON")
+                        base.log(f"{base.yellow}자동 지렁이 민팅: {base.green}켜짐")
                         process_mint_worm(data=data, proxies=proxies)
                     else:
-                        base.log(f"{base.yellow}Auto Mint Worm: {base.red}OFF")
+                        base.log(f"{base.yellow}자동 지렁이 민팅: {base.red}꺼짐")
 
-                    # Break egg
+                    # 알 깨기
                     if self.auto_break_egg:
-                        base.log(f"{base.yellow}Auto Break Egg: {base.green}ON")
+                        base.log(f"{base.yellow}자동 알 깨기: {base.green}켜짐")
                         process_break_egg(data=data, proxies=proxies)
                     else:
-                        base.log(f"{base.yellow}Auto Break Egg: {base.red}OFF")
+                        base.log(f"{base.yellow}자동 알 깨기: {base.red}꺼짐")
 
-                    # Upgrade egg
+                    # 알 업그레이드
                     if self.auto_upgrade_egg:
-                        base.log(f"{base.yellow}Auto Upgrade Egg: {base.green}ON")
+                        base.log(f"{base.yellow}자동 알 업그레이드: {base.green}켜짐")
                         process_upgrade(data=data, proxies=proxies)
                     else:
-                        base.log(f"{base.yellow}Auto Upgrade Egg: {base.red}OFF")
+                        base.log(f"{base.yellow}자동 알 업그레이드: {base.red}꺼짐")
 
                     get_info(data=data, proxies=proxies)
 
                 except Exception as e:
-                    base.log(f"{base.red}Error: {base.white}{e}")
+                    base.log(f"{base.red}오류: {base.white}{e}")
 
             print()
             wait_time = 60 * 60
-            base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
+            base.log(f"{base.yellow}{int(wait_time/60)}분 대기!")
             time.sleep(wait_time)
 
 
