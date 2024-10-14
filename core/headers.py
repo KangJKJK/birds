0fcc33192f0e7d41602039e99282427acf570b1f
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-14 17:51:30 +0900

    Update headers.py

--- core/headers.py
+++ core/headers.py
@@ -1,9 +1,18 @@
 def headers(tele_auth=None, auth=None):
     headers = {
         "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US;q=0.6,en;q=0.5",
+        "Content-Type": "application/json",
         "Origin": "https://birdx.birds.dog",
         "Referer": "https://birdx.birds.dog/",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
     if tele_auth:
