0a94b99feecb537b2b2efd4c6707a1ac9ec7d365
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-14 17:55:55 +0900

    Update headers.py

--- core/headers.py
+++ core/headers.py
@@ -1,24 +1,29 @@
+import platform
+from hokireceh_claimer import base
+
 def headers(tele_auth=None, auth=None):
     headers = {
         "Accept": "application/json, text/plain, */*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US;q=0.6,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9",
         "Content-Type": "application/json",
         "Origin": "https://birdx.birds.dog",
         "Referer": "https://birdx.birds.dog/",
-        "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        "Sec-Ch-Ua": '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
         "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Ch-Ua-Platform": f'"{platform.system()}"',
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-site",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
     }
 
     if tele_auth:
-        headers["Telegramauth"] = f"tma {tele_auth}"
+        headers["Telegramauth"] = tele_auth
+        base.log(f"{base.white}Telegramauth 헤더 추가됨: {base.green}{tele_auth[:10]}...")
 
     if auth:
-        headers["Authorization"] = f"tma {auth}"
+        headers["Authorization"] = f"Bearer {auth}"
+        base.log(f"{base.white}Authorization 헤더 추가됨: {base.green}Bearer {auth[:10]}...")
 
     return headers
